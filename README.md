0b48dce33444743488dc2e1114ea102edd21195e
daVincere <daVincere@users.noreply.github.com>
2023-08-17 06:06:18 +0000

    jekyll build from Action b71a5ca087a3235132babb01cdddb334931f133c

---
+++ README.md
@@ -0,0 +1,9 @@
+## To Do
+
+- [x] Add a blog and a post
+- [ ] Add Archive
+- [ ] Add a page for side projects
+- [ ] Add a page to direct to a resume
+- [ ] Add Google Analytics
+- [ ] Add a page to direct to contact me
+- [ ] Add Algolia Search
---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,31 @@
+var store = [{
+        "title": "The Edelweiss Manifesto :cherry_blossom:",
+        "excerpt":"what is? why, and why not?! I was first introduced to the word edelweiss in a Youtube video from a channel called “History Buffs’’. History Buffs’ creator Nik Hodges critiques historical movies for their accuracy and fiction – in the process presenting a commentary on the history and culture of...","categories": ["blog"],
+        "tags": ["philosophy"],
+        "url": "/blog/2023/03/24/the-edelweiss-manifesto.html",
+        "teaser": null
+      },{
+        "title": "The Path is the Goal",
+        "excerpt":"Playing infinite game in a finite world. NOTE TO THE READER: This article is not an attempt to change you or refute your way of life. The goal of this article is to share what wisdom I’ve found in the pursuit of the same ol’ success and habit formation in...","categories": ["blog"],
+        "tags": ["philosophy, life-lessons"],
+        "url": "/blog/2023/04/11/the-path-is-the-goal.html",
+        "teaser": null
+      },{
+        "title": "The Way of Product Management",
+        "excerpt":"How to become great at the abstract job that is product management. Product management is the job of managing and solving the abstract. There are a lot of beautiful anecdotes out there to simplify the definition but this is the most recent and interesting one that I’ve come across. It’s...","categories": ["blog"],
+        "tags": ["philosophy, product, product-management"],
+        "url": "/blog/2023/05/15/the-way-of-product-management.html",
+        "teaser": null
+      },{
+        "title": "Why TheCareerMint.com",
+        "excerpt":"A note on why thecareermint.com exists On the back of every NCERT book, there used to be a talisman on decision-making by Mahatma Gandhi. It goes something like this: ..Whenever you are in doubt, or when the self becomes too much with you, apply the following test. Recall the face...","categories": ["blog"],
+        "tags": ["product, business, idea, announcement"],
+        "url": "/blog/2023/07/01/thecareermint-com.html",
+        "teaser": null
+      },{
+        "title": "The Way of Hashiras (pillars)",
+        "excerpt":"Why you should do everything, everyday. It was a four-day weekend which was spent working on a B2C platform (mentioned in this post). Towards the end of the last night of this four-day weekend, India’s Independence Day, all efforts turned moot and a new B2B pivot emerged. To discuss the...","categories": ["blog"],
+        "tags": ["philosophy"],
+        "url": "/blog/2023/08/15/the-way-of-hashiras-or-pillars.html",
+        "teaser": null
+      }]
