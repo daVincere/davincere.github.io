4ddacc51841b5946cb2fa9837c35054fcab35e46
hasime x <hasime@limitless.local>
2024-03-02 05:51:08 +0530

    chore(_sites):added _site to git history for artifact in deploy.yml

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,37 @@
+var store = [{
+        "title": "The Edelweiss Manifesto :cherry_blossom:",
+        "excerpt":"what is? why, and why not?! I was first introduced to the word edelweiss in a Youtube video from a channel called “History Buffs’’. History Buffs’ creator Nik Hodges critiques historical movies for their accuracy and fiction – in the process presenting a commentary on the history and culture of...","categories": ["blog"],
+        "tags": ["philosophy"],
+        "url": "/blog/2023/03/25/the-edelweiss-manifesto.html",
+        "teaser": null
+      },{
+        "title": "The Path is the Goal",
+        "excerpt":"Playing infinite game in a finite world. NOTE TO THE READER: This article is not an attempt to change you or refute your way of life. The goal of this article is to share what wisdom I’ve found in the pursuit of the same ol’ success and habit formation in...","categories": ["blog"],
+        "tags": ["philosophy, life-lessons"],
+        "url": "/blog/2023/04/12/the-path-is-the-goal.html",
+        "teaser": null
+      },{
+        "title": "The Way of Product Management",
+        "excerpt":"How to become great at the abstract job that is product management. Product management is the job of managing and solving the abstract. There are a lot of beautiful anecdotes out there to simplify the definition but this is the most recent and interesting one that I’ve come across. It’s...","categories": ["blog"],
+        "tags": ["philosophy, product, product-management"],
+        "url": "/blog/2023/05/16/the-way-of-product-management.html",
+        "teaser": null
+      },{
+        "title": "Why TheCareerMint.com",
+        "excerpt":"A note on why thecareermint.com exists On the back of every NCERT book, there used to be a talisman on decision-making by Mahatma Gandhi. It goes something like this: ..Whenever you are in doubt, or when the self becomes too much with you, apply the following test. Recall the face...","categories": ["blog"],
+        "tags": ["product, business, idea, announcement"],
+        "url": "/blog/2023/07/02/thecareermint-com.html",
+        "teaser": null
+      },{
+        "title": "The Way of Hashiras (pillars)",
+        "excerpt":"Why you should do everything, everyday. Yes, I’ve stolen the word – “Hashira” from the beautifully animated, a lil rushed but overall great anime : Demon Slayer: Kimetsu no Yaiba. If you haven’t seen it, you definitely should. To those unaware, the word “Hashira” means a pillar. With that off...","categories": ["blog"],
+        "tags": ["philosophy"],
+        "url": "/blog/2023/08/16/the-way-of-hashiras-or-pillars.html",
+        "teaser": null
+      },{
+        "title": "Lessons from Feb 2024",
+        "excerpt":"tl;dr of the lessons from this month A. From the Books I’m Reading This is Marketing by Seth Godlin On Marketing “People don’t want to buy a quarter-inch drill bit. They want a quarter-inch hole” Decision matrix: Desire for gain or avoidance of loss. Perception drives reality. On user behaviour:...","categories": ["blog"],
+        "tags": ["retro, monthly"],
+        "url": "/blog/2024/02/29/lessons-feb-2024.html",
+        "teaser": null
+      }]
